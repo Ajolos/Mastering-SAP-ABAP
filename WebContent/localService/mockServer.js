635598ee046bb940e7885c93bd012465cba75fea
Ajolos <d.pacynko@gmail.com>
2018-06-05 22:16:10 +0200

    Create mockServer.js

---
+++ WebContent/localService/mockServer.js
@@ -0,0 +1,15 @@
+sap.ui.define(["sap/ui/core/util/MockServer"], function(MockServer){
+ "use strict";
+  return {
+   init: function(){
+    var oMockServer = new MockServer({
+    rootUri: "/"
+   });
+   oMockServer.simulate("../localService/metadata.xml", {
+   sMockdataBaseUrl: "../localService/mockdata",
+   bGenerateMissingMockData: true
+  });
+  oMockServer.start();
+  }
+ }
+}
