e482c9cd0ed0b624e6aece12baf64c4e733d41a3
Ajolos <d.pacynko@gmail.com>
2019-05-21 20:32:47 +0200

    Update Journey.js

--- Chapter 8/WebContent/test/integration/Journey.js
+++ Chapter 8/WebContent/test/integration/Journey.js
@@ -1,20 +1,21 @@
-jQuery.sap.require("sap.ui.qunit-css");
-jQUery.sap.require("sap.ui.thirdparty.qunit");
+jQuery.sap.require("sap.ui.thirdparty.qunit");
 jQuery.sap.require("sap.ui.qunit.qunit-junit");
-Qunit.config.autostart = false;
+jQuery.sap.require("sap.ui.qunit.QUnitUtils");
+QUnit.config.autostart = false;
 sap.ui.require([
- "sap/ui/test/Opa5",
- "my/namespace/test/integration/pages/BasicArrangements",
- “my/namespace/test/integration/pages/SearchView], function(Opa5, Arrangements){
- "use strict";
- Opa5.extendConfig({
-   arrangements: new Arrangements(),
-   viewNamespaece: "my.namespace.views",
-   autoWait: true
- });
- sap.ui.require([
-  "my/namespace/test/integration/SearchViewJourney"
- ], function(){
-  QUnit.start();
- });
+	"sap/ui/test/Opa5", 
+	"my/namespace/test/integration/pages/BasicArrangements",
+	"my/namespace/test/integration/pages/SearchView"
+], function (Opa5, Arrangements) {
+	"use strict";
+	Opa5.extendConfig({
+		arrangements: new Arrangements(),
+		viewNamespaece: "my.namespace.views",
+		autoWait: true
+	});
+	sap.ui.require([
+		"my/namespace/test/integration/SearchViewJourney"
+	], function () {
+		QUnit.start();
+	});
 });
