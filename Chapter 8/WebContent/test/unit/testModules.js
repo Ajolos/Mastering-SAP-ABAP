632f7b6ff39b1170da74f6445cbaea180e81a809
Ajolos <d.pacynko@gmail.com>
2019-05-21 20:35:01 +0200

    Update testModules.js

--- Chapter 8/WebContent/test/unit/testModules.js
+++ Chapter 8/WebContent/test/unit/testModules.js
@@ -1,25 +1,24 @@
 sap.ui.define([
- "test/unit/simpleTest",
- "test/unit/simpleTest2"], function(){
- "use strict";
+	"my/namespace/test/unit/testModules"
+], function () {
+	"use strict";
 });
 
 sap.ui.require([
- "path/to/file/tested1",
- "path/to/file/tested2",
- . . .
- ],
- function(file1, file2, ... ){
-  "use strict";
-  QUnit.module("Simple module");
-  
-  function testCase(assert, argument, expected){
-   var result = file1.method1(argument);
-   assert.stricEqual(result, expected, "Correct result");
-  }
-  
-  QUnit.test("Name of the test case", function(assert){
-   testCase.call(this, assert, "testArgument1", "expectedValue");
-  });
- . . .
-});
+		"my/namespace/controllers/SearchView.controller"
+	],
+	function (file1) {
+		"use strict";
+		QUnit.module("Simple module");
+
+		function testCase(assert, argument, expected) {
+			var result = (new file1()).testMe(argument);
+			assert.strictEqual(result, expected, "Correct result");
+		}
+
+		QUnit.test("Name of the test case", function (assert) {
+			testCase.call(this, assert, 1, 2);
+			testCase.call(this, assert, -3, -6);
+		});
+
+	});
